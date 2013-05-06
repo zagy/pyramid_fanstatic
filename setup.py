103d50b2102dcf7272b56b9c153096d18dfd4d4a
Godefroid Chapelle <gotcha@bubblenet.be>
2013-05-06 11:51:28 +0200

    setup tox and travis
    for python 2.6, 2.7 and 3.3

--- setup.py
+++ setup.py
@@ -1,5 +1,23 @@
 from setuptools import setup, find_packages
 
+
+from setuptools.command.test import test as TestCommand
+import sys
+
+
+class Tox(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import tox
+        errno = tox.cmdline(self.test_args)
+        sys.exit(errno)
+
+
 version = '0.5.dev0'
 
 
@@ -32,10 +50,13 @@ setup(name='pyramid_fanstatic',
           # -*- Extra requirements: -*-
           'pyramid',
           'fanstatic',
+          'which',
       ],
       entry_points="""
       # -*- Entry points: -*-
       [pyramid.scaffold]
       pyramid_fanstatic=pyramid_fanstatic.scaffolds:PyramidFanstaticTemplate
       """,
+      tests_require=['tox'],
+      cmdclass={'test': Tox},
       )
